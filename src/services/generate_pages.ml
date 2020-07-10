815ae046ddc40e10a5c24c1e930ce046d80fb5c5
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2020-07-10 21:42:25 +0700

    Add default template & optimize build pages

--- src/services/generate_pages.ml
+++ src/services/generate_pages.ml
@@ -11,44 +11,51 @@ type dataTemplate = {
   content : Md.t;
 }
 
-let createPages template =
+let getDefaultTemplate =
+  {j|
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{ matter.title }}</title>
+</head>
+<body>
+  {{ children }}
+</body>
+</html>
+|j}
+
+let createPages (meta : Generate_metadata.t) page (content : Md.t) =
   let open Js.Promise in
-  match template.status with
-  | true ->
-      Fs_Extra.readFile template.page "utf-8"
-      |> then_ (fun res ->
-             Liquid.compile res
-               [%bs.obj
-                 {
-                   matter = template.content.data.matter;
-                   children = template.content.children;
-                 }])
-      |> then_ (fun res -> Fs_Extra.outputFile template.meta.distPath res)
-  | false -> () |> resolve
+  Liquid.compile page
+    [%bs.obj { matter = content.data.matter; children = content.children }]
+  |> then_ (fun res -> Fs_Extra.outputFile meta.distPath res)
+  |> catch (fun err ->
+         errorBanner "Error when trying to create page" err |> resolve)
 
 let selectTemplate (pages : string array) (meta : Generate_metadata.t)
     (content : Md.t) =
   let open Js.Promise in
   Belt.Array.map pages (fun page ->
       let pageFilter = Node.Path.basename page in
-      let templateName = function None -> "index" | Some x -> x in
-      let templateRes =
-        if pageFilter = templateName content.data.matter.template ^ ".html" then
-          { status = true; meta; page; content }
-        else { status = false; meta; page; content }
-      in
-      createPages templateRes)
-  |> resolve
+      let templateName = function None -> "default" | Some x -> x in
+      match
+        pageFilter = templateName content.data.matter.template ^ ".html"
+      with
+      | true ->
+          Fs_Extra.readFile page "utf-8"
+          |> then_ (fun res -> createPages meta res content)
+      | false -> createPages meta getDefaultTemplate content)
 
 let compilePages pages =
   let open Js.Promise in
   Fs_Extra.ensureDir ([| cwd process; "dist" |] |> Path.join |> Path.normalize)
   |> then_ (fun _ -> getPages "templates/*.html")
-  |> then_ (fun templatePages -> templatePages |> resolve)
   |> then_ (fun x ->
          Belt.Array.map pages (fun page ->
              let content, meta = page in
-             selectTemplate x meta content)
+             selectTemplate x meta content |> resolve)
          |> all)
 
 let run pages =
