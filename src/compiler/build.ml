05d09e7243dbba2b5e240dad4f34b015e7885adb
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2020-07-11 15:07:34 +0700

    fix build time + flow

--- src/compiler/build.ml
+++ src/compiler/build.ml
@@ -5,10 +5,10 @@ let getPages (pattern : string) = getGlob pattern
 
 let run () =
   let time = now performance in
-  let _ = Clean.cleanFolder in
   let open Js.Promise in
-  getPages "pages/**/*.md"
+  Clean.cleanFolder |> resolve
+  |> then_ (fun _ -> getPages "pages/**/*.md")
   |> then_ (fun res -> Generate_metadata.run res |> resolve)
   |> then_ (fun res ->
          [| Generate_pages.run res; Generate_assets.copyAssets |] |> all)
-  |> then_ (fun _ -> now performance -. time |> logMeasure |> resolve)
+  |> then_ (fun _ -> time |> logMeasure |> resolve)
